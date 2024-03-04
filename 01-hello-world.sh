7cf7046f3627fee7303011178537de88cc89578e
harispringboot9 <kanta.sriharinaidu@gmail.com>
2024-03-04 17:25:52 +0530

    hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo "hi i am learning devops with aws "
+persion1=ramesh
+persion2=suresh
+
+
+echo "$persion1:hi $persion2, good morning"
+echo "$persion2:good morning echo "$persion1"
+echo "$persion1: how are you $persion2"
+echo "$persion2: i am good, how are you $persion1 "
