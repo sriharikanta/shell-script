d463b08d5e3b64b1f95e0acfeec685f859c988d2
harispringboot9 <kanta.sriharinaidu@gmail.com>
2024-03-04 22:54:25 +0530

    hello-date-tme script added

--- 05-variable.sh
+++ 05-variable.sh
@@ -4,10 +4,10 @@ echo "please enter the username"
 
 read username
 
-echo "enter username is $username"
+echo "enter username is = $username"
 
 echo "please enter the password"
 
-read password
+read -s password
 
-echo "enter password is $password"
+echo "enter password is = $password"
