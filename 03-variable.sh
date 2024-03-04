f74f4fc37740dbbf8ca950da4adc99e48722c81d
harispringboot9 <kanta.sriharinaidu@gmail.com>
2024-03-04 21:55:37 +0530

    hello-date-tme script added

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+
+echo "hi i am learning devops with aws "
+persion1=$1
+persion2=$2
+
+
+echo "$persion1: hi $persion2, good morning"
+echo "$persion2: good morning  $persion1"
+echo "$persion1: how are you $persion2"
+echo "$persion2: i am good, how are you $persion1"
