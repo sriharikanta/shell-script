1e956db92be092fddf0841024ff44f0320542e14
harispringboot9 <kanta.sriharinaidu@gmail.com>
2024-03-04 17:30:07 +0530

    hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -5,6 +5,6 @@ persion2=suresh
 
 
 echo "$persion1: hi $persion2, good morning"
-echo "$persion2: good morning echo $persion1"
+echo "$persion2: good morning  $persion1"
 echo "$persion1: how are you $persion2"
 echo "$persion2: i am good, how are you $persion1"
